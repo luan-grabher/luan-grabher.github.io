023c5d63d51e61e822753542060b04eee79636c8
Luan Grabher <52000832+luan-grabher@users.noreply.github.com>
2020-01-23 23:18:45 -0300

    Add files via upload

---
+++ Infojobs/Auto Infojobs.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Auto Infojobs
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http*://*.infojobs.com.br/*
+// @grant        none
+// @require      http://luan-grabher.github.io/Infojobs/vendor/jquery.js
+// @require      http://luan-grabher.github.io/Infojobs/vendor/functions.js
+// @require      http://luan-grabher.github.io/Infojobs/Model/PageVaga_Model.js
+// @require      http://luan-grabher.github.io/Infojobs/Model/PageVagasDeEmprego_Model.js
+// @require      http://luan-grabher.github.io/Infojobs/Control/Pages_Control.js
+// @require      http://luan-grabher.github.io/Infojobs/AppControl.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
